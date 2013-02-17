4d79aeee135c80fa8bf9f9a9d33e934bf5ad450e
Daniel Scharrer <daniel@constexpr.org>
2013-02-17 14:49:37 +0100

    Add support for codepages without aliases in iconv

--- src/util/load.cpp
+++ src/util/load.cpp
@@ -24,48 +24,162 @@
 #include <map>
 #include <sstream>
 #include <algorithm>
+#include <iomanip>
 
 #include <iconv.h>
 #include <errno.h>
+
+#include <boost/foreach.hpp>
 
 #include "util/log.hpp"
 
 namespace {
 
-static const boost::uint32_t cp_utf8 = 65001;
-static const boost::uint32_t cp_utf16 = 1200;
+static const boost::uint32_t cp_utf8  = 65001;
+static const boost::uint32_t cp_ascii = 20127;
 static const char replacement_char = '_';
 
 std::map<boost::uint32_t, iconv_t> converters;
 
-iconv_t get_converter(boost::uint32_t codepage) {
+static size_t get_encoding_size(boost::uint32_t codepage) {
+	switch(codepage) {
+		case  1200: return 2u; // UTF-16LE
+		case  1201: return 2u; // UTF-16BE
+		case 12000: return 4u; // UTF-32LE
+		case 12001: return 4u; // UTF-32BE
+		default:    return 1u;
+	}
+}
+
+//! Get names for encodings where iconv doesn't have the codepage alias
+static const char * get_encoding_name(boost::uint32_t codepage) {
+	switch(codepage) {
+		case   708: return "ISO-8859-6";
+		case   936: return "GBK";
+		case   949: return "UHC";
+		case   950: return "BIG5";
+		// iconv's behavior for "UTF-16" is platform-dependant if there is no BOM.
+		// There never is any BOM in Inno Setup files and it's always little-endian,
+		// so we specify the exact encoding.
+		case  1200: return "UTF-16LE";
+		case  1201: return "UTF-16BE";
+		case  1252: return "MS-ANSI";
+		case  1361: return "JOHAB";
+		case 10000: return "MACINTOSH";
+		case 10002: return "BIG5";
+		case 10008: return "GB2312";
+		case 12000: return "UTF-32LE";
+		case 12001: return "UTF-32BE";
+		case 20003: return "IBM5550";
+		case 20127: return "US-ASCII";
+		case 20261: return "T.61";
+		case 20269: return "ISO_6937";
+		case 20273: return "IBM273";
+		case 20277: return "IBM277";
+		case 20278: return "IBM278";
+		case 20280: return "IBM280";
+		case 20284: return "IBM284";
+		case 20285: return "IBM285";
+		case 20290: return "IBM290";
+		case 20297: return "IBM297";
+		case 20420: return "IBM420";
+		case 20423: return "IBM423";
+		case 20424: return "IBM424";
+		case 20866: return "KOI8-R";
+		case 20871: return "IBM871";
+		case 20880: return "IBM880";
+		case 20905: return "IBM905";
+		case 20924: return "IBM1047";
+		case 20932: return "EUC-JP-MS";
+		case 20936: return "EUC-CN";
+		case 21025: return "IBM1025";
+		case 21866: return "KOI8-U";
+		case 28591: return "ISO-8859-1";
+		case 28592: return "ISO-8859-2";
+		case 28593: return "ISO-8859-3";
+		case 28594: return "ISO-8859-4";
+		case 28595: return "ISO-8859-5";
+		case 28596: return "ISO-8859-6";
+		case 28597: return "ISO-8859-7";
+		case 28598: return "ISO-8859-8";
+		case 28599: return "ISO-8859-9";
+		case 28603: return "ISO-8859-13";
+		case 28605: return "ISO-8859-15";
+		case 38598: return "ISO-8859-8";
+		case 50220: return "ISO-2022-JP";
+		case 50221: return "ISO-2022-JP-2";
+		case 50222: return "ISO-2022-JP-3";
+		case 50225: return "ISO-2022-KR";
+		case 50227: return "ISO-2022-CN";
+		case 50229: return "ISO-2022-CN-EXT";
+		case 50930: return "EBCDIC-JP-E";
+		case 51932: return "EUC-JP";
+		case 51936: return "EUC-CN";
+		case 51949: return "EUC-KR";
+		case 51950: return "EUC-CN";
+		case 54936: return "GB18030";
+		case 65000: return "UTF-7";
+		case 65001: return "UTF-8";
+		default: return NULL;
+	}
+}
+
+static iconv_t get_converter(boost::uint32_t codepage) {
 	
+	// Try to reuse an existing converter if possible
 	std::map<boost::uint32_t, iconv_t>::iterator i = converters.find(codepage);
-	
 	if(i != converters.end()) {
 		return i->second;
 	}
 	
-	std::ostringstream oss;
-	if(codepage == cp_utf16) {
-		// iconv's behavior for "UTF-16" is platform-dependant if there is no BOM.
-		// There never is any BOM in Inno Setup files and it's always little-endian,
-		// so we specify the exact encoding.
-		oss << "UTF-16LE";
-	} else {
-		oss << "CP" << codepage;
+	iconv_t handle = iconv_t(-1);
+	
+	const char * encoding = get_encoding_name(codepage);
+	if(encoding) {
+		handle = iconv_open("UTF-8", encoding);
 	}
 	
-	iconv_t handle = iconv_open("UTF-8", oss.str().c_str());
+	// Otherwise, try a few different codepage name prefixes
+	if(handle == iconv_t(-1)) {
+		const char * prefixes[] = { "MSCP", "CP", "WINDOWS-", "MS", "IBM", "IBM-", "" };
+		BOOST_FOREACH(const char * prefix, prefixes) {
+			std::ostringstream oss;
+			oss << prefix << std::setfill('0') << std::setw(3) << codepage;
+			handle = iconv_open("UTF-8", oss.str().c_str());
+			if(handle != iconv_t(-1)) {
+				break;
+			}
+		}
+	}
 	
 	if(handle == iconv_t(-1)) {
-		log_warning << "could not get " << oss.str() << " -> UTF-8 converter";
+		log_warning << "could not get codepage " << codepage << " -> UTF-8 converter";
 	}
 	
 	return converters[codepage] = handle;
 }
 
-};
+//! Fallback conversion that will at least work for ASCII characters
+static void to_utf8_fallback(const std::string & from, std::string & to,
+                             boost::uint32_t codepage) {
+	
+	size_t skip = get_encoding_size(codepage);
+	
+	to.clear();
+	to.reserve(ceildiv(from.size(), skip));
+	
+	for(size_t i = 0; i < from.size(); i += skip) {
+		if((unsigned char)from[i] <= 127) {
+			// copy ASCII characters
+			to.push_back(from[i]);
+		} else {
+			// replace everything else with underscores
+			to.push_back(replacement_char);
+		}
+	}
+}
+
+} // anonymous namespace
 
 void binary_string::load(std::istream & is, std::string & target) {
 	
@@ -93,29 +207,9 @@ void encoded_string::load(std::istream & is, std::string & target, boost::uint32
 	to_utf8(temp, target, codepage);
 }
 
-//! Fallback conversion that will at least work for ASCII characters
-static void to_utf8_fallback(const std::string & from, std::string & to,
-                             boost::uint32_t codepage) {
-	
-	size_t skip = ((codepage == cp_utf16) ? 2u : 1u);
-	
-	to.clear();
-	to.reserve(ceildiv(from.size(), skip));
-	
-	for(size_t i = 0; i < from.size(); i += skip) {
-		if((unsigned char)from[i] <= 127) {
-			// copy ASCII characters
-			to.push_back(from[i]);
-		} else {
-			// replace everything else with underscores
-			to.push_back(replacement_char);
-		}
-	}
-}
-
 void to_utf8(const std::string & from, std::string & to, boost::uint32_t codepage) {
 	
-	if(codepage == cp_utf8) {
+	if(codepage == cp_utf8 || codepage == cp_ascii) {
 		// copy UTF-8 directly
 		to = from;
 		return;
@@ -151,7 +245,7 @@ void to_utf8(const std::string & from, std::string & to, boost::uint32_t codepag
 	
 	iconv(converter, NULL, NULL, NULL, NULL);
 	
-	size_t skip = ((codepage == cp_utf16) ? 2u : 1u);
+	size_t skip = get_encoding_size(codepage);
 	
 	bool warn = false;
 	
