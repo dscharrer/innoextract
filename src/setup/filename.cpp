fdf73d8d973b3e0b67fbe84d5d6ac94f6322ec17
Daniel Scharrer <daniel@constexpr.org>
2018-06-11 10:10:31 +0200

    [clang-tidy] filename: Remove redundant static keyword

--- src/setup/filename.cpp
+++ src/setup/filename.cpp
@@ -53,7 +53,7 @@ struct is_unsafe_path_char {
 	}
 };
 
-static std::string replace_unsafe_chars(const std::string & str) {
+std::string replace_unsafe_chars(const std::string & str) {
 	std::string result;
 	result.resize(str.size());
 	std::replace_copy_if(str.begin(), str.end(), result.begin(), is_unsafe_path_char(), '$');
