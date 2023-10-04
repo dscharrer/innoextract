b41b6edbef5883ebe4b8805331e4223ce6718de1
piotr-markiewicz-mobica <138652687+piotr-markiewicz-mobica@users.noreply.github.com>
2023-10-04 14:27:40 +0200

    logging improvements (#56)

--- tests/scripts/run_post_test.sh
+++ tests/scripts/run_post_test.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
+tag=$1
+user=$2
+current_date=$(date +%s)
+log_path="/home/runner/logs/$current_date-$tag-$user"
+
 #date and version number taken from .version file
-date=$(cat "/home/runner/build/.version" | awk '{ print $1 }')
-version=$(cat "/home/runner/build/.version" | awk '{ print $2 }')
+version_file="/home/runner/build/.version"
+date=$(cat $version_file | awk '{ print $1 }')
+version=$(cat $version_file | awk '{ print $2 }')
 
 echo "Date of the build: $date"
 echo "Current build version: $version"
+echo "Log path: $log_path"
+
 
 cp -r /home/runner/innoextract-wasm/tests/output /home/runner/build
 cd /home/runner/build/output && ln -s log.html index.html
 sed -i "s,\"title\":\"Task log,\"title\":\"Task log for build: "$version",g" /home/runner/build/output/log.html
 
+mkdir -p $log_path
+cp -rf /home/runner/build/output/* $log_path
+cp $version_file $log_path
+echo "Done."
 
-mkdir -p "/home/runner/logs/$date-$version"
-cp -rf /home/runner/build/output/* "/home/runner/logs/$date-$version"
-echo "Done"
